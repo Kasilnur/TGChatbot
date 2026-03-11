dd835037721c3f19e8088a1b980a70e39fa5391e
Kasilnur <newlega2021@gmail.com>
2026-03-11 18:34:37 +0500

    add AI and Speech Recognizer

--- src/BotService/main.py
+++ src/BotService/main.py
@@ -3,7 +3,14 @@ from telebot import types
 import os
 from dotenv import load_dotenv
 import speech_recognition as sr
-import soundfile as sf
+from pydub import AudioSegment
+from huggingface_hub import InferenceClient
+
+# Инициализация путей для папок
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+VOICE_DIR = os.path.join(BASE_DIR, '..', 'TempAudio')
+if not os.path.exists(VOICE_DIR):
+    os.makedirs(VOICE_DIR)
 
 # Загрузка переменных окружения
 load_dotenv()
@@ -14,6 +21,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 user_sessions = {}
 
+
 def get_full_commands():
     return [
         types.BotCommand("/start", "Запустить бота"),
@@ -21,10 +29,40 @@ def get_full_commands():
         types.BotCommand("/exit", "Завершить работу")
     ]
 
+
 def get_start_only():
     return [
         types.BotCommand("/start", "Запустить бота")
     ]
+
+
+# Инициализация клиента
+client = InferenceClient(token=os.getenv("DEEPSEEK_TOKEN"))
+
+
+def get_ai_response(prompt):
+    completion = client.chat.completions.create(
+        model="deepseek-ai/DeepSeek-R1:novita",
+        messages=[
+            {
+                "role": "system",
+                "content": """Ты — лаконичный помощник в чате telegram.
+                 Сразу подавай общение на уровне хороших друзей.
+                 Выдавай ответ сразу без рассуждений и тегов.
+                 Не дублируй ответ.
+                 В ответе не пытайся использовать Markdown.""" # Добавить под API
+            },
+                {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+    )
+    print(f"""
+    LOGGER
+    Ответ LLM: {completion.choices[0].message.content}
+    """)
+    return completion.choices[0].message.content
 
 # Слушатели
 @bot.message_handler(commands=['start'])
@@ -36,9 +74,11 @@ def start(message):
         scope=types.BotCommandScopeChat(chat_id)
     )
     user_sessions[message.from_user.id] = True
-    bot.reply_to(message, "Приветствую, Я Иру! В любой момент ты можешь написать мне. Благодаря, мне  Помогу чем смогу!")
+    bot.reply_to(
+        message, "Приветствую, Я Иру! В любой момент ты можешь написать мне. Благодаря, мне  Помогу чем смогу!")
 
-    bot.set_message_reaction(message.chat.id, message.message_id, [telebot.types.ReactionTypeEmoji('🤝')])
+    bot.set_message_reaction(message.chat.id, message.message_id, [
+                             telebot.types.ReactionTypeEmoji('🤝')])
 
 
 @bot.message_handler(commands=['help'])
@@ -63,55 +103,82 @@ def exit_command(message):
     chat_id = message.chat.id
 
     if user_sessions.get(message.from_user.id) is False:
-        bot.send_message(message.chat.id, "Я тебя впервые вижу, напиши /start, познакомимся 🙂")
+        bot.send_message(
+            message.chat.id, "Я тебя впервые вижу, напиши /start, познакомимся 🙂")
         return
-    
+
     user_sessions[message.from_user.id] = False
     bot.set_my_commands(
         get_start_only(),
         scope=types.BotCommandScopeChat(chat_id)
     )
-    bot.send_message(message.chat.id, "До связи! Если я снова понадоблюсь, напиши /start")
+    bot.send_message(
+        message.chat.id, "До связи! Если я снова понадоблюсь, напиши /start")
+
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
     if user_sessions.get(message.from_user.id) is False:
-        bot.send_message(message.chat.id, "Я тебя впервые вижу. Напиши /start, познакомимся 🙂")
+        bot.send_message(
+            message.chat.id, "Я тебя впервые вижу. Напиши /start, познакомимся 🙂")
         return
     if message.text == "Справка":
         help_command(message)
     elif message.text == "Выход":
         exit_command(message)
     else:
-        bot.send_message(message.chat.id, "Я пока только учусь понимать обычный текст. Попробуй меню!")
+        bot.send_chat_action(message.chat.id, 'typing')
+        bot.send_message(message.chat.id, get_ai_response(message.text))
+
 
 @bot.message_handler(content_types=['voice'])
 def handle_voice(message):
-    # Скачивание голосового сообщения
-    file_info = bot.get_file(message.voice.file_id)
-    downloaded_voice = bot.download_file(file_info.file_path)
+    bot.set_message_reaction(message.chat.id, message.message_id, [types.ReactionTypeEmoji("👀")])
+    try:
+        # Скачивание голосового сообщения
+        file_info = bot.get_file(message.voice.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
 
-    with open(os.path.join(os.path., '\\..\\VoiceData\\voice.ogg'), 'wb') as voice:
-        voice.write(downloaded_voice)
+        ogg_filename = os.path.join(VOICE_DIR, f"{message.from_user.id}.ogg")
+        wav_filename = os.path.join(VOICE_DIR, f"{message.from_user.id}.wav")
 
-    # Конвертация файла
-    # data, samplerate = sf.read('voice.ogg')
-    # sf.write('wav_voice.wav', data, samplerate)
+        # Сохранение скачанного файла в формат ogg
+        with open(ogg_filename, 'wb') as new_file:
+            new_file.write(downloaded_file)
 
-    # recognizer = sr.Recognizer()
-    # with sr.AudioFile('wav_voice.wav') as file:
-    #     audio_data = recognizer.record(file)
-    #     try:
-    #         text = recognizer.recognize_bing(audio_data, language="ru-RU")
-    #         bot.reply_to(message, f"Вы сказали: {text}")
-    #     except:
-    #         bot.reply_to(message, f"Я не совсем понял, что вы сказали")
- 
+        # Конвертация OGG в WAV
+        audio = AudioSegment.from_file(ogg_filename)
+        audio.export(wav_filename, format="wav")
+
+        # Распознавание текста
+        recognizer = sr.Recognizer()
+        with sr.AudioFile(wav_filename) as source:
+            audio_data = recognizer.record(source)
+            text = recognizer.recognize_google(audio_data, language="ru-RU")
+            bot.send_chat_action(message.chat.id, 'typing')
+            bot.send_message(message.chat.id, get_ai_response(text))
+
+            print(f"""LOGGER\nТекст голосового сообщения от пользователя {message.from_user.id}: {text}""")
+
+    except Exception as e:
+        print(f"Ошибка: {e}")
+        bot.reply_to(message, "Я не совсем понял, что вы сказали")
+
+    finally:
+        if os.path.exists(ogg_filename):
+            os.remove(ogg_filename)
+        if os.path.exists(wav_filename):
+            os.remove(wav_filename)
+
+
 bot.set_my_commands([
     types.BotCommand("/start", "Запустить бота"),
     types.BotCommand("/help", "Показать справку"),
     types.BotCommand("/exit", "Завершить работу")
 ])
 
-# Запуск бота
-bot.infinity_polling(timeout=5)
+
+if __name__ == "__main__":
+    # Запуск бота
+    print("Бот запущен")
+    bot.infinity_polling(none_stop=True, interval=0, logger_level=0, skip_pending=True)
